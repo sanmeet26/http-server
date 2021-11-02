8a3c97294bcdab8d2219a7f35917baa3130385fe
Sanmeet Wakchaure <wakchauresr19.comp@coep.ac.in>
2021-11-02 16:46:25 +0000

    Replace http-server.py

--- http-server.py
+++ http-server.py
@@ -1,74 +1,132 @@
 '''
-	Implementation of simple HTTP server
+    HTTP Server Implementation
 '''
 
-from socket import *
+import socket
+import sys
+import os
 
-# this function takes request and gives response to it
-def handle_request(request):
-	# find filename from headers
-	headers = request.split("\n")
-	filename = headers[0].split()[1]
-	#print(filename)
-	
-	# if browser requests root of server, then index.html content will be there in response
-	if filename == '/':
-		filename = "/index.html"
-	
-	# try to open the requested file from htdocs folder
-	# if file not found, then open notfound.html file and complete the response
-	try:
-		fp = open("htdocs" + filename, "r")
-		content = fp.read()
-		fp.close()
-		response = "HTTP/1.1 200 OK\n\n" + content
-		
-	except FileNotFoundError:
-		#response = "HTTP/1.1 404 NOT FOUND\n\nFile not found"
-		fp = open("htdocs/notfound.html", "r")
-		content = fp.read()
-		fp.close()
-		response = "HTTP/1.1 404 NOT FOUND\n\n" + content
-		
-	return response
+# request methods
+valid_methods = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE']
+
+# status codes
+status_codes = {'100': 'Continue', '200' : 'OK', '201': 'Created', '204': 'No Content', '301': 'Moved Permanently', '304': 'Not Modified', '400': 'Bad Request', '404': 'Not Found', '501': 'Not Implemented'}
+
+def manage_request(client_request):
+    # parse the request
+    request_line = client_request.split('\n')[0].split()
+
+    request_method = None
+    request_path = None
+    http_version = None
+
+    # checking the length of request line
+    if len(request_line) == 3:
+        request_method = request_line[0]
+        request_path = request_line[1]
+        http_version = request_line[2]
+    elif len(request_line) == 2:
+        request_method = request_line[0]
+        request_path = request_line[1]
+    else:
+        request_method = request_line[0]
+
+    # method validation
+    if request_method.upper() not in valid_methods:
+        print("Method is not valid")
+        sys.exit(1)
+
+    file_name = request_path
+    
+    # create response with headers and content body
+    # if resource is present then use '200 OK', else use '404  NOT FOUND'
+    if file_name == '/':
+        fp = open("htdocs/index.html", 'r')
+        content = fp.read()
+        fp.close()
+        response = http_version + " 200 " + status_codes['200'] + "\nServer: myServer\n\n" + content
+    else:
+        try:
+            if file_name == "/notfound.html":
+                raise Exception
+            fp = open("htdocs"+file_name, 'r')
+            content = fp.read()
+            fp.close()
+            response = http_version + " 200 " + status_codes['200'] + "\nServer: myServer\n\n" + content
+        except:
+            fp = open("htdocs/notfound.html", 'r')
+            content = fp.read()
+            fp.close()
+            response = http_version + " 404 " + status_codes['404'] + "\nServer: myServer\n\n" + content
+
+    print(response)
+
+    # return the created response    
+    return response
+        
+
+def start_server(server_socket):
+    while True:
+        # initiate the connection with the client
+        client_socket, client_address = server_socket.accept()
+
+        print("Connected to", client_address)
+
+        # recieve the request from client and decode it
+        request = client_socket.recv(1024).decode()
+        if request == "":
+            continue
+        print(request)
+
+        # create the response
+        response = manage_request(request)
+
+        # send the encoded response to client
+        client_socket.send(response.encode())
+
+        # close the connection with the client
+        client_socket.close()
+        # print("Connection closed")
 
 
-# server name and port number
-SERVER_NAME = '0.0.0.0'
-SERVER_PORT = 12001
+def create_server_socket():
+    # create a TCP socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# creating socket and binding it to the port
-server_socket = socket(AF_INET, SOCK_STREAM)
-server_socket.bind((SERVER_NAME, SERVER_PORT))
+    # assign server name and server port
+    # here it is localhost
+    SERVER_NAME = ''
+    SERVER_PORT = 12001
 
-# server keeps on listening
-server_socket.listen(4)
-print("Listening on port %s ..."%SERVER_PORT)
+    try:
+        # set the condition for port reusability whenever server is restarted
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-while True:
-	# keep accepting connection for any client 
-	client_socket, client_address = server_socket.accept()
-	
-	# recieve and print the request by client
-	request = client_socket.recv(2048).decode()
-	print(request)
-	
-	# make the response
+        # bind the server socket with port number
+        server_socket.bind((SERVER_NAME, SERVER_PORT))
 
-	# if request is empty string, then go to next iteration (i.e. look for next client)
-	if request == "":
-		continue
-
-	# call function for handling request
-	response = handle_request(request)
-	
-	# send created response to client
-	client_socket.send(response.encode())
-	
-	# close client connection
-	client_socket.close()
-
-# close server socket
-server_socket.close()
+        # allow the server to listen to incoming connections
+        server_socket.listen(1)
+        print("Listening on port", SERVER_PORT)
+    except Exception as err:
+        # calling sys functions to get error details
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type)
+        print("Error occured in", fname, "at line no.", exc_tb.tb_lineno, ":")
+        print("\t", err)
+        sys.exit(1)
+    
+    # return the server socket created
+    return server_socket
 
 
+if __name__ == "__main__":
+
+    # make the server socket
+    server_socket = create_server_socket()
+
+    # start the server
+    start_server(server_socket)
+
+    print(server_socket)
